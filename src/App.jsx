5a35343136587ead21bb400beb3c77313b2ea7e1
ozgedogru <ozgenurkarademir@gmail.com>
2025-02-19 12:09:07 +0300

    adds all pages w/react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import ComparisonPage from "./pages/ComparisonPage";
+import MeteogramPage from "./pages/MeteogramPage";
+import Header from "../src/layouts/Header";
+import WeatherNews from "./pages/WeatherNews";
 
 function App() {
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <Router>
+      <div className="flex flex-col h-screen">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/meteogram" replace />} />
+          <Route path="/meteogram" element={<MeteogramPage />} />
+          <Route path="/comparison" element={<ComparisonPage />} />
+          <Route path="/news" element={<WeatherNews />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
